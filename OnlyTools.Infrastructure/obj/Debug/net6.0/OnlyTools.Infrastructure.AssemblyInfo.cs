1e4fef63022f7650b6513b21ac31b1ea67b2b404
MaryPeteva <shinosedai404@gmail.com>
2024-04-02 20:16:43 +0300

    added myTools page

--- OnlyTools.Infrastructure/obj/Debug/net6.0/OnlyTools.Infrastructure.AssemblyInfo.cs
+++ OnlyTools.Infrastructure/obj/Debug/net6.0/OnlyTools.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlyTools.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55d9e5eb1d66ee2a5a87897f7595110e0959e733")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7dd73736bf22b6503c7e1e903f40321fac35142")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlyTools.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlyTools.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
